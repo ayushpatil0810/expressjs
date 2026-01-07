5f95793b652daf0480d52674833c986d79d2495f
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-07 16:39:13 +0530

    feat: implement CRUD operations for books in the bookstore API

--- bookstore/index.js
+++ bookstore/index.js
@@ -8,6 +8,10 @@ const books = [
 
 const app = express()
 
+// Middlewares
+app.use(express.json())
+
+// Routes
 app.get('/books', (req, res) => {
     res.setHeader('x-developer', 'Ayush')
     res.json(books)
@@ -19,7 +23,7 @@ app.get('/books/:id', (req, res) => {
     if(isNaN(id))
         return res
         .status(400)
-        .json({error: `Id must be of type number`})
+        .json({error: "Id must be of type number"})
 
     const book = books.find(e => e.id === id) // SELECT * from books where id  = {id}
     
@@ -30,6 +34,41 @@ app.get('/books/:id', (req, res) => {
     return res.json(book)
 })
 
+app.post('/books', (req, res) => {
+    const { title, author } = req.body
+
+    if(!title || title === "")
+        return res.status(400).json({'error': 'title is required'})
+    
+    if(!author || author === "")
+        return res.status(400).json({'error': 'author is required'})
+    
+    const id = books.length + 1;
+    const book = {id, title, author}
+
+    books.push(book)
+
+    return res.status(201).json({'message': "Book Created successfully", id})
+})
+
+app.delete('/books/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    
+    if (isNaN(id)) 
+        return res.status(400).json({error: "Id must be of type number"})
+
+    const indexToDelete = books.findIndex(e => e.id === id)
+
+    if (indexToDelete === -1) 
+        return res
+        .status(404)
+        .json({error: `Book with id ${id} does not exists!`})
+
+    books.splice(indexToDelete, 1)
+
+    return res.status(200).json({'message' : "Book Deleted successfully"})
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}.`)
